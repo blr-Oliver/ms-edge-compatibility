cf20cade2711726950eeb94021c00891df631524
blr-Oliver <vasily.liaskovsky@gmail.com>
2019-02-12 19:20:46 +0300

    testable version

--- docs/fullscreen/fullscreen-test.js
+++ docs/fullscreen/fullscreen-test.js
@@ -1,1 +1,8 @@
-angular.module('fullscreen-test', ['fullscreen']);
+angular.module('fullscreen-test', ['fullscreen'])//
+.controller('RootCtrl', ['fullscreen', '$scope', function(fullscreen, $scope){
+  var self = this;
+  $scope.fullscreen = fullscreen;
+  fullscreen.$watch(function(active, manual){
+    self.isFullscreen = active && !manual;
+  });
+}]);
